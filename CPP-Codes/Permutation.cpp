d125e3f0f25aa63ee9654112fe71368064f6b7de
Aakansh Dixit <aakanshdixit@gmail.com>
2026-07-01 23:00:25 +0530

    Implement permutation generation in Permutation.cpp

---
+++ CPP-Codes/Permutation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void f(int index, vector<int>& nums, vector<vector<int>>& ans){
+        int n=nums.size();
+        if(index==n){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<n;i++){
+            swap(nums[i],nums[index]);
+            f(index+1,nums,ans);
+            swap(nums[i],nums[index]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        f(0,nums,ans);
+        return ans;
+    }
+};
