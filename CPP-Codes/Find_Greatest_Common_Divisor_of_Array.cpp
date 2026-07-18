670492d61bc328b47aff2d339520368790be7350
Aakansh Dixit <aakanshdixit@gmail.com>
2026-07-18 14:57:19 +0530

    Implement GCD finder for an array of integers

---
+++ CPP-Codes/Find_Greatest_Common_Divisor_of_Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int n=nums.size();
+
+        sort(nums.begin() , nums.end());
+        int a=nums[0];
+        int b = nums[n-1];
+
+        return gcd(a , b);
+
+    }
+};
