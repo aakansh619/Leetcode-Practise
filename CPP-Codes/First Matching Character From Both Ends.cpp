822c082366f9f3725f6245039e3d9494e3e6aec1
Aakansh Dixit <aakanshdixit@gmail.com>
2026-08-25 00:15:23 +0530

    Implement firstMatchingIndex to find matching characters

---
+++ CPP-Codes/First Matching Character From Both Ends.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        int l=0; int r=s.size()-1;
+
+        while(l<=r){
+            if(s[l] != s[r]){
+                l++;
+                r--;
+            }
+            if(s[l] == s[r]){
+                return l;
+            }
+        }
+        return -1;
+    }
+};
